4185eaeac4619fea106f68cfc85124db8fdffe54
Bryant Chandler <brychanrobot@gmail.com>
2017-01-10 19:14:24 -0700

    added animated transitions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import '../main.css'
 
 class Main extends Component {
     render () {
         return (
             <div className='main-container'>
-                {this.props.children}
+                <ReactCSSTransitionGroup
+                    transitionName='appear'
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}>
+                    {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+                </ReactCSSTransitionGroup>
             </div>
         )
     }
